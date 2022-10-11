0175da74de3997697b82a943d92007282f617e54
satheesh <satheeshkumar.tr@gmail.com>
2022-10-11 12:02:50 +0400

    Initial commit of Projects

---
+++ Fund_Relief_Management_System/F-R-M-S-Blockchain/ui/components/homepage.vue
@@ -0,0 +1,88 @@
+<template>
+    <v-app>
+        <v-container class="form">
+            <v-form>
+            <v-row>
+                <v-col></v-col>
+                <v-col>
+                    <v-row class="space">
+                    
+                        <v-text class="text">
+                            Fund Relif Managment System
+                        </v-text>
+                    </v-row>
+
+                    <v-row>
+                    <v-col></v-col>
+                        <v-btn text @click="user()" class= "box">User Login</v-btn>
+                    <v-col></v-col>
+                    </v-row>
+
+                    
+                    <v-row>
+                     <v-col></v-col>
+                        <v-btn text @click="admin()"class= "box"> Admin Login</v-btn>
+                     <v-col></v-col>
+                    </v-row>
+                              
+                </v-col>
+                <v-col></v-col>
+            </v-row>
+            </v-form>
+        </v-container>
+
+     
+    </v-app>
+</template>
+<script>
+    import Web3 from 'xdc3'
+    export default{
+        name: "homepage",
+        data:()=> ({
+
+        }),
+        methods:{
+            async sendmoney(){
+                if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Xinfin browser detected. You should consider trying XDCpay"
+      );
+    }
+            },
+            async admin(){
+                this.$router.push('/adminlogin')
+            },
+            async user(){
+                this.$router.push('/userlogin')
+            }
+            
+        }
+    }
+</script>
+
+
+<style>
+.space{
+margin-top:10em;
+
+}
+
+.box{
+border:1px solid white;
+width: 50%;
+margin-top:2em;
+margin-right:30px;
+
+
+}
+
+.text{
+font-size:25px;
+text-shadow: 1px 2px white;
+
+}
+</style>
---
+++ Fund_Relief_Management_System/F-R-M-S-Blockchain/ui/pages/adminlogin.vue
@@ -0,0 +1,60 @@
+<template >
+    <v-container class="form">
+        <v-form>
+            <v-col>
+                <v-row >
+                    <v-text class="lol">Admin Login</v-text>
+                </v-row>
+              
+              
+                <v-row>
+                    <v-text-field placeholder="username" v-model="username"></v-text-field>
+                </v-row>
+                <v-row>
+                    <v-text-field placeholder="password" v-model="password"></v-text-field>
+                </v-row>
+                <v-row>
+                    <v-btn block @click="signin()"> Sign In</v-btn>
+                </v-row>
+            </v-col>
+        </v-form>
+    </v-container>
+</template>
+<script>
+    export default{
+        name:"adminlogin",
+        data:() =>({
+            username:"",
+            password :"",
+        }),
+        methods:{
+            async signin(){
+                let login = {
+                    username: this.username,
+                    password :this.password
+                }
+                let res = await this.$axios.post("http://127.0.0.1:8000/adminlogin",login)
+                if(res.data==true){
+                    this.$router.push('/admin')
+                }
+            }
+        }
+    }
+</script>
+<style>
+    .form{
+        width:500px;
+        margin:10% auto;
+        border:1px solid grey;
+        box-shadow: 3px 3px grey;
+        
+    }
+    
+
+    .lol{
+    font-size:20px;
+    font-weight:400;
+    }
+ 
+   
+</style>
